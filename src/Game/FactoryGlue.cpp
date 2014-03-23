502aa6726499748b35d5cfcf9b39a878f63923ef
Canadadry <moureyjerome-spam@yahoo.fr>
2014-03-23 16:23:52 +0100

    Fix gravity bug

--- src/Game/FactoryGlue.cpp
+++ src/Game/FactoryGlue.cpp
@@ -6,6 +6,8 @@
 
 #include <Graphics/RessourceManager.h>
 
+#include <iostream>
+
 static std::wstring toWide(std::string string)
 {
     return std::wstring(string.begin(),string.end());
@@ -28,12 +30,14 @@ bool PlateformerPhysicBuilder::selfInitilalise(void* context)
     if(context == NULL) return false;
     JSONObject&   jsonObject = *(JSONObject*) context;
 
-    gravity           = sf::Vector2f( getValue<float>(jsonObject,"gavityX",0.0f),getValue<float>(jsonObject,"gavityY",0.0f) );
+    gravity           = sf::Vector2f( getValue<float>(jsonObject,"gravityX",0.0f),getValue<float>(jsonObject,"gravityY",0.0f) );
     max_speed         = sf::Vector2f( getValue<float>(jsonObject,"maxSpeedX",0.0f),getValue<float>(jsonObject,"maxSpeedY",0.0f));
     jump_power        = getValue<float>(jsonObject,"jumpPower",0.0f);
     walk_speed        = getValue<float>(jsonObject,"walkSpeed",0.0f);
     run_speed         = getValue<float>(jsonObject,"runSpeed",0.0f);
     walk_deceleration = getValue<float>(jsonObject,"walkDeceleration",0.0f);
+
+    std::cout << "gravity ("<<gravity.x<<","<<gravity.y<<")" << std::endl;
 
 
     return true;
